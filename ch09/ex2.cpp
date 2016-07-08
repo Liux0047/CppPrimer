e4bee62ed8b729d1487bf8b0163b00e0f673d547
Allen <tracy1kobe@gmail.com>
2016-07-08 23:00:16 +0800

    ch09 ex2

---
+++ ch09/ex2.cpp
@@ -0,0 +1,14 @@
+/*
+Define a list that holds elements that are deques that hold ints.
+*/
+
+#include <list>
+#include <deque>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	list<deque<int>> li;
+	return 0;
+}
